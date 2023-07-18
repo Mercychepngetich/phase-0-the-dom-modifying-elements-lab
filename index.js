3138d8447c74ccacd88e6ec90ccb0a52400eaa55
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-07-19 00:29:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.querySelector('#main').remove();
+let newHeader = document.createElement('h1')
+newHeader.id ="victory"
+newHeader.innerHTML="Mercy is the champion"
+
